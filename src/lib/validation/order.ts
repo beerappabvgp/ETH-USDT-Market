94730800bad277a36303a7ccecb41bb0c398fb4e
beerappabvgp <beerappabharathb@gmail.com>
2024-12-16 14:18:17 +0530

    Added orders model and zod validation

---
+++ src/lib/validation/order.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const OrderSchema = z.object({
+    asset: z.string().min(3, "Asset name is too short ... "),
+    type: z.enum(["buy", "sell"]),
+    price: z.number().positive("Price must be positive."),
+    quantity: z.number().positive("Quantity must be greater than 0"),
+    expiration: z.string().refine((date) => !isNaN(Date.parse(date)), {
+        message: "Invallid date format",
+    }),
+});
+
